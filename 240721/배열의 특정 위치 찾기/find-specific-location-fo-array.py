70f22a74a629487c82fe29e6816bf753b1d567d8
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-21 21:13:19 +0900

    Title: 배열의 특정 위치 찾기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240721/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,4 +3,4 @@ lst = list(map(int, input().split()))
 sum_nums = lst[1::2]
 average_nums = lst[2::3]
 
-print(sum(sum_nums), sum(average_nums)/len(average_nums))
+print(sum(sum_nums), round(sum(average_nums)/len(average_nums),1))
