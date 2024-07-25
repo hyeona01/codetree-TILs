377316223d8e5d466e629da003bd9ed3e0a41378
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-25 12:14:56 +0900

    Title: 사칙연산 함수, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,25 @@
+def plus(a, b):
+    return a+b
+
+def minus(a, b):
+    return a-b
+
+def times(a, b):
+    return a*b
+
+def divide(a, b):
+    return a//b
+
+    
+a, o, c = input().split()
+a=int(a)
+c=int(c)
+
+if o == '+':
+    print(a, o, c, "=", plus(a, c))
+if o == '-':
+    print(a, o, c, "=", minus(a, c))
+if o == '*':
+    print(a, o, c, "=", times(a, c))
+if o == '/':
+    print(a, o, c, "=", divide(a, c))
