d6085c82d32d76835831dd991e52d5a4b226c39b
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-26 14:44:59 +0900

    Title: 두 정수에 대한 연산값 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240726/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240726/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,12 +1,8 @@
 def solution(a, b):
     if a>b:
         return b+10, a*2
-    else:
-        return a+10, b*2
 
 a,b = map(int, input().split())
 
-result = solution(a,b)
-
-for n in result:
-    print(n, end=" ")
+x, y = solution(a,b)
+print(max(x,y), min(x,y))
