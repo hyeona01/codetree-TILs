f64a391f541a3c3c2cfccea0cdc8b6d7fbc9fd71
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-26 14:29:24 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,14 @@
+def solution(A):
+    al = A[0]
+    cnt = A.count(al)
+
+    for i in range(cnt):
+        A.replace(al, '')
+
+    return A
+
+A = input()
+if solution(A) == '':
+    print("No")
+else:
+    print("Yes")
