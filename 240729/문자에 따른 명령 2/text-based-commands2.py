49a48af3331b248969b700c6c132dc0d1cdbab0b
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-29 22:49:08 +0900

    Title: 문자에 따른 명령 2, Time: 239ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/문자에 따른 명령 2/text-based-commands2.py
+++ 240729/문자에 따른 명령 2/text-based-commands2.py
@@ -4,17 +4,14 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 dir = input()
-
-nx, ny = 0, 0
+dir_num = 0
 for i in dir:
     if i == 'L':
-        nx -= 1
-        ny -= 1
+        dir_num -= 1
     elif i == 'R':
-        nx += 1
-        ny += 1
+        dir_num += 1
     else:
-        x += dx[nx%4]
-        y += dy[ny%4]
+        x += dx[dir_num%4]
+        y += dy[dir_num%4]
 
 print(x, y)
