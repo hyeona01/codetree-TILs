80b0894d9321229ad3494358bbafec486747d78d
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-29 22:40:47 +0900

    Title: 문자에 따른 명령 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240729/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,20 @@
+x, y = 0, 0
+# 방향
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+dir = input()
+
+nx, ny = 0, 0
+for i in dir:
+    if i == 'L':
+        nx -= 1
+        ny -= 1
+    elif i == 'R':
+        nx += 1
+        ny += 1
+    else:
+        x = dx[nx%4]
+        y = dy[ny%4]
+
+print(x, y)
