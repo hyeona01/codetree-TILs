0c2ae0e1592c841f73891ed7f9134c8b6831e1f0
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-07-29 22:21:55 +0900

    Title: 방향에 맞춰 이동, Time: 175ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/방향에 맞춰 이동/move-in-direction.py
+++ 240729/방향에 맞춰 이동/move-in-direction.py
@@ -1,15 +1,23 @@
 N = int(input())
 x, y = 0, 0
 
+# W, S, N, E
+dx, dy = [-1,0,0,1], [0,-1,1,0]
+
 for i in range(N):
     d, n = input().split()
-    if d == 'N':
-        y += int(n)
-    elif d == 'E':
-        x += int(n)
+    dir = 0 # 방향
+
+    n = int(n)
+    if d == 'W':
+        dir = 0
     elif d == 'S':
-        y -= int(n)
-    elif d == 'W':
-        x -= int(n)
+        dir = 1
+    elif d == 'N':
+        dir = 2
+    elif d == 'E':
+        dir = 3
+
+    x, y = x+dx[dir]*n, y+dy[dir]*n
 
 print(x, y)
