34ea5b8a16d895e226845aa0fe653480b40efe58
Hyeona Seol <126868156+hyeona01@users.noreply.github.com>
2024-08-02 16:25:04 +0900

    Title: 컨베이어 벨트, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/컨베이어 벨트/conveyor-belt.py
+++ 240802/컨베이어 벨트/conveyor-belt.py
@@ -2,10 +2,11 @@ n, t = map(int,input().split())
 belt = list(map(int, input().split()))
 belt += list(map(int, input().split()))
 
-temp = belt[-1]
-for i in range(n*2-2, -1, -1):
-    belt[i+1] = belt[i]
-belt[0] = temp
+for time in range(t):
+    temp = belt[-1]
+    for i in range(n*2-2, -1, -1):
+        belt[i+1] = belt[i]
+    belt[0] = temp
 
 for i in range(2):
     for j in range(n):
